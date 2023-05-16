8b9689d0132ecdfde396f275576e592a9a140baf
Exequiel Pagliari <exepa@hotmail.com>
2023-05-16 20:59:11 -0300

    Prueba

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,16 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            mensaje: 'Hola Mundo con Vue!',
-            curso: 'Codo a Codo'
+            terminal: `Te he despertado del estasis y teletransportado a un planeta donde necesito que termines unos trabajos. Te encuentras en Lh'owon, el planeta natal de los S'pht.
+
+Estoy seguro que estarás maravillado por saber que ocurrió mientras estuviste fuera por tanto tiempo, que ocurrió con $IMarathon$i y Tau Ceti, y sobre todo dónde está tu Lanzamisiles y tu Pistola de Fusión. Habrá tiempo para explicaciones después.
+            
+Necesito que obtengas acceso a la red computacional del Pfhor de este planeta. Tu misión es conectar dos circuitos Uplink en diferentes ubicaciones dentro del complejo.
+
+Ten cuidado, estoy seguro que ya reconociste algunos de nuestros viejos amigos.`,
         }
     }
 }).mount('#app')
+
+
+
